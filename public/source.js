2ce52e7d4b74e241fee701d35e4d217d4af66081
blaughlin <bernardlaughlin@gmail.com>
2025-02-05 08:54:42 -0900

    removed print statements

--- public/source.js
+++ public/source.js
@@ -4,9 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const submitBtn = document.getElementById("translate");
     if (submitBtn) {
         submitBtn.addEventListener("click", async () => {
-            console.log("Button clicked!");
-
-
             await getAIResponse();
         });
     }
@@ -30,7 +27,6 @@ async function getAIResponse() {
     let selectedLanguage = document.querySelector('input[name="language"]:checked')?.value;
 
     const prompt = `You are a ${selectedLanguage} translator. Translate the following text: "` + document.getElementById("input").value + '" to ' + `${selectedLanguage}. Do not explain anything just give the translation`
-    console.log("Prompt:", prompt);
 
     try {
         const response = await fetch("https://backend-fyif.onrender.com/chat", {
@@ -42,7 +38,6 @@ async function getAIResponse() {
         if (!response.ok) throw new Error(`Server error: ${response.status}`);
 
         const data = await response.json();
-        console.log(data.message)
         if (data && data.message) {
             document.getElementById("result-area").style.display = "block";
             document.getElementById("translation-area").style.display = "none";
